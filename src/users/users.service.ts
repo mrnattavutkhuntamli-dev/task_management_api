b63d96da3ba7bd49f593bdac833ccda1a0fe6975
DESKTOP-14NDB6S\MYPC101 <mr.nattavut.khuntamli@gmail.com>
2026-04-01 22:10:41 +0700

    ทำ controller และ service ของ user เพิ่ม function create  users สำเร็จ แต่ยังขาดบางส่วนที่ไม่ได้เพิ่มเช่นเพิ่มรูปโปรไฟล์

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,3 @@
-import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -10,8 +9,18 @@ export class UsersService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ) {}
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    // ตรวจสอบ email นี้ว่ามีอยู่ใน ตาราง ของ users แล้วหรือยัง
+    const existingUser = await this.userRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+    if (existingUser) {
+      throw new Error('อีเมลนี้มีอยู่แล้ว');
+    }
+    // สร้าง user โดย entity จะทำ Hash Password ให้เองผ่าน @BeforeInsert
+    const user = this.userRepository.create(createUserDto);
+    // บันทึกข้อมูลลงตาราง users
+    return await this.userRepository.save(user);
   }
 
   findAll() {
