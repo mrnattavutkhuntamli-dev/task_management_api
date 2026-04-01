462b528aed1e703e516fea772e33c204ea7607c1
DESKTOP-14NDB6S\MYPC101 <mr.nattavut.khuntamli@gmail.com>
2026-04-01 22:01:45 +0700

    ตั้งค่า dto สำหรับทำ validate ตอน create ของ users

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,30 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  IsOptional,
+  isEnum,
+  IsEnum,
+} from 'class-validator';
+import { UserRole } from '../entities/user.entity';
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail({}, { message: 'อีเมลไม่ถูกต้อง' })
+  email: string;
+
+  @IsString()
+  @MinLength(6, { message: 'รหัสผ่านต้องมีอย่างน้อย 6 ตัวอักษร' })
+  password: string;
+
+  @IsEnum(UserRole)
+  @IsOptional()
+  role?: UserRole;
+
+  @IsString()
+  @IsOptional()
+  avatar?: string;
+}
