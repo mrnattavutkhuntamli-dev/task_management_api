12c602d541c7591cf7ac7392721edad9a027cc97
DESKTOP-14NDB6S\MYPC101 <mr.nattavut.khuntamli@gmail.com>
2026-04-01 22:09:54 +0700

    ตั้งค่า main และ set routes api เป็น api/v1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // เปิดใช้งาน Validation ทั่วทั้ง App
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Perfix สำหรับ API ที่ใช้งานกับ NestJS
+  app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
