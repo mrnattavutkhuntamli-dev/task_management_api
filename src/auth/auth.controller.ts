14c94fabbfc80215db71a675fdf270169134455a
DESKTOP-14NDB6S\MYPC101 <mr.nattavut.khuntamli@gmail.com>
2026-04-02 16:24:49 +0700

    เพิ่ม function getprofile เพื่อที่จะดึงข้อมูล token จากการ login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,24 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Request,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+
+interface RequestWithUser extends Request {
+  user: {
+    userId: string;
+    email: string;
+    role: string;
+  };
+}
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
@@ -10,5 +28,11 @@ export class AuthController {
   async login(@Body() loginDto: LoginDto) {
     // แนะนำให้สร้าง LoginDto เพื่อความปลอดภัย
     return this.authService.login(loginDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getProfile(@Request() req: RequestWithUser) {
+    return req.user;
   }
 }
