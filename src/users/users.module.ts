f5d21eda911445ccde213a2b877a0aba2e261373
DESKTOP-14NDB6S\MYPC101 <mr.nattavut.khuntamli@gmail.com>
2026-04-10 09:19:28 +0700

    ปรับปรุง module task management

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,8 +4,9 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { ProfileController } from './profile.controller';
+import { ActivitiesModule } from 'src/activities/activities.module';
 @Module({
-  imports: [TypeOrmModule.forFeature([User])], // บรรทัดนี้สำคัญที่สุด!
+  imports: [TypeOrmModule.forFeature([User]), ActivitiesModule], // บรรทัดนี้สำคัญที่สุด!
   controllers: [UsersController, ProfileController],
   providers: [UsersService],
   exports: [UsersService], // บรรทัดนี้สำคัญที่สุด!
